45fdc43bc2d7cf2153af88406a425bfb7fc0a500
Colin Lu <landcolin@gmail.com>
2015-12-16 18:23:34 +0800

    update p

--- SMKF_upd.m
+++ SMKF_upd.m
@@ -1,7 +1,6 @@
 function [x_upd_SMKF, P_upd_SMKF, S_x_upd_SMKF] = SMKF_upd(z_pseudo, x_pred_SMKF, P_pred_SMKF, S_x_pred_SMKF, S_z_pseudo, H, r, N)
 
-% p = 0.5;
-p = get_p(P_pred_SMKF, S_x_pred_SMKF, S_z_pseudo, H, r, N);
+p = get_p(P_pred_SMKF, S_x_pred_SMKF, S_z_pseudo);
 
 K = ( (1 + inv(p) ) * S_x_pred_SMKF * H' + P_pred_SMKF * H') * inv( (1 + inv(p) ) * H * S_x_pred_SMKF * H' + (1 + p) * S_z_pseudo + H * P_pred_SMKF * H' + r / N);
 I = eye(size(K, 1) );
